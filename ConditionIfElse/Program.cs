59e3fd3cba45a523ae0f77ac33dd379d21182d88
AnnaT <augenblick21@mail.ru>
2022-10-15 16:47:46 +0300

    add 005 IfElse

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "анна")
+{
+    Console.WriteLine("Ура,это же Анна!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
